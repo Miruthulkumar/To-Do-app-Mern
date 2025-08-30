c1c20b0f34bfca50d9b63ee7f00e5bdf40fd2bea
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-08-30 22:06:58 +0530

    idk

--- TODOLIST/server.js
+++ TODOLIST/server.js
@@ -2,22 +2,71 @@ const express = require("express");
 const app = express();
 const PORT = 2000;
 
+let todos = [
+  [
+    {
+      id: 1,
+      title: "Buy groceries",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Finish project report",
+      completed: true,
+    },
+    {
+      id: 3,
+      title: "Book dentist appointment",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "Pay electricity bill",
+      completed: true,
+    },
+    {
+      id: 5,
+      title: "Clean the living room",
+      completed: false,
+    },
+    {
+      id: 6,
+      title: "Prepare for presentation",
+      completed: false,
+    },
+    {
+      id: 7,
+      title: "Read a chapter of a book",
+      completed: true,
+    },
+    {
+      id: 8,
+      title: "Go for evening walk",
+      completed: false,
+    },
+    {
+      id: 9,
+      title: "Fix broken chair",
+      completed: false,
+    },
+    {
+      id: 10,
+      title: "Call insurance agent",
+      completed: true,
+    },
+  ],
+];
+
 //to get all todo items
 app.get("/todos", (req, res) => {
-  res.json([
-    {
-      task: "Go to Gym",
-      priority: "HIGH",
-    },
-    {
-      task: "Water Plants",
-      priority: "HIGH",
-    },
-    {
-      task: "Do Homework",
-      priority: "LOW",
-    },
-  ]);
+  res.status(200).json(todos);
+});
+
+//to get todo by id
+app.get("/todo/:taskId", (req, res) => {
+  const taskId = req.params.taskId;
+  const todo = todos.find((todo) => todo.id == taskId);
+  res.status(200).json(todo);
 });
 
 app.listen(PORT, () => {
