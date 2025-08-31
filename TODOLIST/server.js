4419d66c1e22456e8f344c09e6b13e5469e1ab31
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-08-31 09:06:23 +0530

    gpt copy update and delete

--- TODOLIST/server.js
+++ TODOLIST/server.js
@@ -1,80 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 2000;
-
-// let todos = [
-//   [
-//     {
-//       id: 1,
-//       title: "Buy groceries",
-//       completed: false,
-//     },
-//     {
-//       id: 2,
-//       title: "Finish project report",
-//       completed: true,
-//     },
-//     {
-//       id: 3,
-//       title: "Book dentist appointment",
-//       completed: false,
-//     },
-//     {
-//       id: 4,
-//       title: "Pay electricity bill",
-//       completed: true,
-//     },
-//     {
-//       id: 5,
-//       title: "Clean the living room",
-//       completed: false,
-//     },
-//     {
-//       id: 6,
-//       title: "Prepare for presentation",
-//       completed: false,
-//     },
-//     {
-//       id: 7,
-//       title: "Read a chapter of a book",
-//       completed: true,
-//     },
-//     {
-//       id: 8,
-//       title: "Go for evening walk",
-//       completed: false,
-//     },
-//     {
-//       id: 9,
-//       title: "Fix broken chair",
-//       completed: false,
-//     },
-//     {
-//       id: 10,
-//       title: "Call insurance agent",
-//       completed: true,
-//     },
-//   ],
-// ];
-
-// //to get all todo items
-// app.get("/todos", (req, res) => {
-//   res.status(200).json(todos);
-// });
-
-// //to get todo by id
-// app.get("/todo/:taskId", (req, res) => {
-//   const taskId = req.params.taskId;
-//   const todo = todos.find((todo) => todo.id == taskId);
-//   res.status(200).json(todo);
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`The server is running in ${PORT}`);
-// });
-
-//nirmal anna's code
-
 const express = require("express");
 const app = express();
 const PORT = 3000;
@@ -149,6 +72,30 @@ app.get("/todo/:taskId", (req, res) => {
 app.post("/todo", (req, res) => {
   todos = [...todos, req.body];
   res.status(200).send("Todo created successfully.");
+});
+
+//update todo
+app.put("/todo/update", (req, res) => {
+  const { id, title, completed } = req.body;
+
+  const todo = todos.find((t) => t.id == id);
+  if (!todo) {
+    return res.status(404).send("Todo not found");
+  }
+  todo.name = title;
+  todo.priority = completed;
+  res.status(200).send("Updated successfully");
+});
+
+//delete todo
+app.delete("/todo/delete", (req, res) => {
+  const { id } = req.body;
+  const exist = todos.some((todo) => todo.id == id);
+  if (!exist) {
+    res.status(404).send("task not found");
+  }
+  todos = todos.filter((t) => t.id != id);
+  res.status(200).send("Deleted successfully");
 });
 
 app.listen(PORT, () => {
