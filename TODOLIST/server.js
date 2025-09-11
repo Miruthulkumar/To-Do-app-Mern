5d94c5d460027ca2c802cb4321d8fa80d99194bf
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-09-11 23:12:11 +0530

    Remove unnecessary blank line before the PUT endpoint definition in server.js

--- TODOLIST/server.js
+++ TODOLIST/server.js
@@ -93,7 +93,6 @@ app.post("/todo", async (req, res) => {
   });
 });
 
-
 // Update an existing todo item in the local array
 // Define a PUT API endpoint at "/todo/update" to update a todo
 app.put("/todo/update", (req, res) => {
